4fb173aa8639f47a853ff1bfd6fb3153fecac0ac
Bishnu <bishnuthapako@gmail.com>
2022-03-19 19:10:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names){
+    let message = [];
+    for(let i = 0; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+        // console.log(message)
+    }
+    return message;
+
+    }
+    writeCards(names)
+
+
+
+function countDown(){
+    let counter = 0;
+        while(counter < 11){
+        console.log(counter)
+        counter = counter + 1;
+        }
+    return counter
+    }
+    countDown() 
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+     
+
+
+      /*
+ function countDown(){
+        let counter = 10;
+         while(counter > 0){
+          console.log(counter)
+          counter = counter - 1;
+        }
+        return counter
+      }
+      countDown()
+
+      */
